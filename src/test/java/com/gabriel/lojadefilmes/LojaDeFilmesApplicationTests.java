a58600251251cf6035a4e589f43616333ee8bab8
gabrieltherock <gabrieldarocha2013@hotmail.com>
2021-07-03 13:18:53 -0300

    Initial commit

---
+++ src/test/java/com/gabriel/lojadefilmes/LojaDeFilmesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriel.lojadefilmes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaDeFilmesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
