158ed303be657ed5596821b79668f800ce5691fc
gabrieltherock <gabrieldarocha2013@hotmail.com>
2021-07-04 14:13:13 -0300

    cadastro finalizado

--- src/main/java/com/gabriel/lojadefilmes/controller/AppController.java
+++ src/main/java/com/gabriel/lojadefilmes/controller/AppController.java
@@ -1,9 +1,13 @@
 package com.gabriel.lojadefilmes.controller;
 
+import com.gabriel.lojadefilmes.model.usuario.Usuario;
 import com.gabriel.lojadefilmes.repository.usuario.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AppController {
@@ -11,9 +15,25 @@ public class AppController {
     @Autowired
     UsuarioRepository usuarioRepository;
 
-    @GetMapping(value = "/teste")
+    @GetMapping( "")
     public String paginaInicial() {
         return "index";
     }
 
+    @GetMapping("/cadastro")
+    public String getCadastroForm(Model model) {
+        model.addAttribute(new Usuario());
+        return "cadastroForm";
+    }
+
+    @PostMapping("/cadastro")
+    public String cadastraUsuario(Usuario usuario) {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String encodedPassword = passwordEncoder.encode(usuario.getSenha());
+        usuario.setSenha(encodedPassword);
+
+        usuarioRepository.save(usuario);
+
+        return "cadastroComSucesso";
+    }
 }
